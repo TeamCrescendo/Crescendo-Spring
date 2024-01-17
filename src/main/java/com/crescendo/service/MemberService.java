6104b69b35bdf8593792f593a2e72e6624bace8d
rudrbgus <rudrbgus3@gmail.com>
2024-01-17 15:49:15 +0900

    회원가입 컨트롤러 만드는중

---
+++ src/main/java/com/crescendo/service/MemberService.java
@@ -0,0 +1,25 @@
+package com.crescendo.service;
+
+import com.crescendo.dto.request.SignUpRequestDTO;
+import com.crescendo.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+@Transactional // JPA 사용시 필수 (서비스에)
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+
+    // 회원 가입 처리
+    public void signUp(SignUpRequestDTO dto){
+
+    }
+
+
+}
