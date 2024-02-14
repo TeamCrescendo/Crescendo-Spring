8218fd2362df9e70bf4dab29c13635aa6655d922
rudrbgus <rudrbgus3@gmail.com>
2024-02-14 23:05:02 +0900

    PATCH문도 받게 설정

--- src/main/java/com/crescendo/config/CorsConfig.java
+++ src/main/java/com/crescendo/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry
                 .addMapping("/api/**") // 어떤 요청 URL을 허용할 지
                 .allowedOrigins("http://localhost:3000", "http://crescendo-react-bucket.s3-website.ap-northeast-2.amazonaws.com") // 어떤 클라이언트를 허용할 지
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 어떤 요청 방식을 허용할 지
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // 어떤 요청 방식을 허용할 지
                 .allowedHeaders("*") // 어떤 헤더를 허용할 지
                 .exposedHeaders("score-id")
                 .allowCredentials(true) // 쿠키 전달을 허용할 지 (토큰 도)
